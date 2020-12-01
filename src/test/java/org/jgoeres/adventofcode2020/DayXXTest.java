62726d03f5fc1aa32492856ea0813cc1a408d3c8
Justin Goeres <jgoeres@liveareacx.com>
2020-12-01 10:19:25 -0500

    Add DayXX unit test template

---
+++ src/test/java/org/jgoeres/adventofcode2020/DayXXTest.java
@@ -0,0 +1,31 @@
+package org.jgoeres.adventofcode2020;
+
+import org.jgoeres.adventofcode2020.DayXX.RunDayXX;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DayXXTest {
+    static String DAY = "XX";
+
+//    @Test
+//    public void DayXXA() {
+//        int result = 0;
+//        try {
+//            result = RunDayXX.problemXXA();
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//        Assert.assertEquals(0, result);
+//    }
+//
+//    @Test
+//    public void DayXXB() {
+//        int result = 0;
+//        try {
+//            result = RunDayXX.problemXXB();
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//        Assert.assertEquals(0, result);
+//    }
+}
