807fbfd747f2de09c08423af69e96e711e09aa88
Justin Goeres <jgoeres@liveareacx.com>
2020-12-05 19:30:01 -0500

    Modify serviceTemplate to use a regex instead of int parsing

--- resources/templates/DayXXServiceTemplate.java
+++ resources/templates/DayXXServiceTemplate.java
@@ -22,28 +22,33 @@ public class Day${AOC_DAY}Service {
     public int doPartA() {
         int result = 0;
         /** Put problem implementation here **/
-        
+
         return result;
     }
 
     public int doPartB() {
         int result = 0;
         /** Put problem implementation here **/
-        
+
         return result;
     }
 
-    // load inputs line-by-line as list of Integers 
+    // load inputs line-by-line and apply a regex to extract fields
     private void loadInputs(String pathToFile) {
         inputList.clear();
         try (BufferedReader br = new BufferedReader(new FileReader(pathToFile))) {
             String line;
             Integer nextInt = 0;
+            /** Replace this regex **/
+            Pattern p = Pattern.compile("([FB]{7})([LR]{3})");
             while ((line = br.readLine()) != null) {
                 // process the line.
-                nextInt = Integer.parseInt(line);
-
-                inputList.add(nextInt);
+                Matcher m = p.matcher(line);
+                if (m.find()) { // If our regex matched this line
+                    // Parse it
+                    String field1 = m.group(1);
+                    String field2 = m.group(2);
+                }
             }
         } catch (Exception e) {
             System.out.println("Exception occurred: " + e.getMessage());
