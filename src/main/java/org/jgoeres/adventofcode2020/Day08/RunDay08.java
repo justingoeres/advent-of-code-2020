a1aee429b5ab469f1db854ca608fdfa080a88674
Justin Goeres <jgoeres@liveareacx.com>
2020-12-08 00:03:57 -0500

    Day 8 Init from aocInit.sh

---
+++ src/main/java/org/jgoeres/adventofcode2020/Day08/RunDay08.java
@@ -0,0 +1,35 @@
+package org.jgoeres.adventofcode2020.Day08;
+public abstract class RunDay08 {
+    static String pathToInputs = "data/day08/input.txt";
+
+    static Day08Service day08Service = new Day08Service();
+
+    public static int problem08A() {
+        System.out.println("=== DAY 08A ===");
+
+        int result = day08Service.doPartA();
+
+        System.out.println("Day 08A: Answer = " + result);
+
+//        Day 08A: Answer =
+//        Time elapsed:	xxx ms
+
+        return result;
+    }
+
+    public static int problem08B() {
+        System.out.println("=== DAY 08B ===");
+
+        int result = day08Service.doPartB();
+
+        System.out.println("Day 08B: Answer = " + result);
+
+//        Day {$AOC_DAY}B: Answer =
+//        Time elapsed:	xxx ms
+
+        return result;
+    }
+
+
+
+}
