c455bde9a1011076b449e2558507ed67f6efe767
Justin Goeres <jgoeres@liveareacx.com>
2020-12-10 00:06:38 -0500

    Day 10 Init from aocInit.sh

---
+++ src/main/java/org/jgoeres/adventofcode2020/Day10/RunDay10.java
@@ -0,0 +1,29 @@
+package org.jgoeres.adventofcode2020.Day10;
+public abstract class RunDay10 {
+    static String pathToInputs = "data/day10/input.txt";
+
+    static Day10Service day10Service = new Day10Service();
+
+    public static int problem10A() {
+        System.out.println("=== DAY 10A ===");
+
+        int result = day10Service.doPartA();
+
+        System.out.println("Day 10A: Answer = " + result);
+
+        return result;
+    }
+
+    public static int problem10B() {
+        System.out.println("=== DAY 10B ===");
+
+        int result = day10Service.doPartB();
+
+        System.out.println("Day 10B: Answer = " + result);
+
+        return result;
+    }
+
+
+
+}
---
+++ src/test/java/org/jgoeres/adventofcode2020/Day10Test.java
@@ -0,0 +1,31 @@
+package org.jgoeres.adventofcode2020;
+
+import org.jgoeres.adventofcode2020.Day10.RunDay10;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Day10Test {
+   @Test
+   public void Day10A() {
+       final int EXPECTED = 0;
+       int result = 0;
+       try {
+           result = RunDay10.problem10A();
+       } catch (Exception e) {
+           System.out.println(e.getMessage());
+       }
+       Assert.assertEquals(EXPECTED, result);
+   }
+
+   @Test
+   public void Day10B() {
+       final int EXPECTED = 0;
+       int result = 0;
+       try {
+           result = RunDay10.problem10B();
+       } catch (Exception e) {
+           System.out.println(e.getMessage());
+       }
+       Assert.assertEquals(EXPECTED, result);
+   }
+}
